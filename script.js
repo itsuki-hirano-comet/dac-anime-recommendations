9051279eda313865d65b16f97b9e60aa86e5e68c
itsuki-hirano-comet <itsukihirano212@gmail.com>
2026-04-12 19:09:34 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,19 @@
     let next = true;
     const start = document.querySelector('#start');
     const initialText = document.querySelector('#initial');
-    
+    let resultID;
+    let resultTitle;
+    let resultImage;
+
+    const titles = {
+        aot: 'Attack on Titan',
+        fmab: 'Fullmetal Alchemist Brotherhood',
+        mha: 'My Hero Academia',
+        mob: 'Mob Psycho 100',
+        fate: 'Fate/Stay Night: Unlimited Blade Works',
+        fire: 'Fire Force',
+        killLaKill: 'Kill la Kill'
+    }
 
     window.addEventListener('load', function(){
         start.addEventListener('click', function() {
@@ -46,20 +58,23 @@
                 if (answerClass === 'answerFinal') {  
                     // show result
                     // showResult(answerID);
-                    console.log(`result is ${answerID}`)
+                    // console.log(`result is ${answerID}`);
+                    showResult(answerID);
                 } else {
                     questionSelector(answerID);  
 
                     if (next === true) {
                         changeDisplay(nextQuestion);
                     } else {
-                        console.log(`result is ${answerString}`);
-                        //showResult(answerString);
+                        // console.log(`result is ${answerString}`);
+                        showResult(answerString);
                     }                    
                 }
             });
         }     
     }
+
+    
 
     // split question selector by genre
     function questionSelector(answerID) {
@@ -83,6 +98,80 @@
                 case 'sports': questionSports(); break;
                 case 'drama': questionDrama(); break;
                 case 'mystery': questionMystery(); break;
+                case 'music1': questionMusic1(); break;
+                case 'comedy': questionComedy(); break;
+                case 'sliceOfLife': questionSlice(); break;
+                case 'music2': questionsMusic2();
+            }
+        }
+    }
+
+    function questionsMusic2() {
+        if (counter === 2) {
+            nextQuestion = '-popular';
+        } else if (answers[2] === 'pop') {
+            if (counter === 3) {
+                nextQuestion = '-length'
+            } else {
+                next = false;
+            }
+        } else {
+            nextQuestion = answerString;
+        }
+    }
+
+    function questionSlice() {
+        if (counter === 2) {
+            nextQuestion = '-slice';
+        } else if (answers[2] === 'sliceRelax') {
+            if (counter === 3) {
+                nextQuestion = '-world';
+            } else if (answers[3] === 'fantasyWorld') {
+                next = false;
+            } else {
+                nextQuestion = answerString;
+            }
+        } else {
+            nextQuestion = answerString;
+        }
+    }
+
+    function questionComedy() {
+        if (counter === 2) {
+            nextQuestion = '-popular';
+        } else if (answers[2] === 'pop') {
+            if (counter === 3) {
+                nextQuestion = '-jokes';
+            } else if (answers[3] === 'nsfw') {
+                nextQuestion = answerString;
+            } else {
+                next = false;
+            }
+        } else {
+            if (counter === 3) {
+                nextQuestion = '-emotion';
+            } else if (answers[3] === 'notEmotional' && answers[4] === 'subculture') {
+                if (counter === 5) {
+                    nextQuestion = '-jokes';
+                } else {
+                    next = false;
+                }
+            } else {
+                nextQuestion = answerString;
+            }
+        }
+    }
+
+    function questionMusic1() {
+        if (counter === 2) {
+            nextQuestion = '-popular';
+        } else if (answers[2] === 'niche') {
+            nextQuestion = answerString;
+        } else if (answers[2] === 'pop') {
+            if (counter === 3) {
+                nextQuestion = '-length';
+            } else {
+                next = false;
             }
         }
     }
@@ -279,6 +368,8 @@
                 next = false;
             }else if (counter === 5 && answers[2] === 'niche' && answers[3] === 'completed' && answers[4] === 'serious') {
                 next = false;
+            } else if (counter === 6 && answers[2] === 'niche' && answers[3] === 'completed' && answers[4] === 'comedic' && answers[5] === 'animation') {
+                next = false;
             } else {
                 nextQuestion = answerString;
             }
@@ -287,15 +378,26 @@
 
     function showResult(answer) {
         const result = document.querySelector(`#result`);
+        const titleDisplay = document.querySelector('#result h3'); 
         
-        
+        console.log(answer);
+
+        resultSelection(answer);
+
+        // console.log(`titles.${resultID}`);
+        // resultTitle = titles.resultID;
+        // resultImage = images.resultID;
+        console.log(resultTitle);
+        console.log(resultImage);
 
         // result.innerHTML += `<img src="images/img${answerString}.jpg" class="resultImg">`;
         // result.innerHTML += '<button id="reset">Give me another anime</button>';
 
         //add h3 anime title
+        titleDisplay.innerHTML = `${resultTitle}`;
 
-        result.innerHTML += `<div class="side"><img src="images/img${answerString}.jpg" class="resultImg"> <button id="reset">Give me another anime</button></div>`
+
+        result.innerHTML += `<div class="side"><img src="images/${resultImage}.jpg" class="resultImg"> <button id="reset">Give me another anime</button></div>`
 
         setTimeout(function(){
             result.className = 'resultOn';
@@ -312,8 +414,37 @@
         //     document.querySelector('#reset').addEventListener('click', function(){
         //         location.reload();
         //     });
-        // })
-        
-        
+        // })   
+    }
+
+    function resultSelection(answer) {
+        switch (answer) {
+            case '-type1-action-pop-completed-serious-growPower':
+                resultTitle = titles.aot;
+                resultImage = 'aot';
+                break;
+            case '-type1-action-pop-completed-serious-growMature':
+                resultTitle = titles.fmab;
+                resultImage = 'fmab';
+                break;
+            case '-type1-action-pop-completed-comedic-growPower':
+                resultTitle = titles.mha;
+                resultImage = 'mha';
+                break;
+            case '-type1-action-pop-completed-comedic-growMature': 
+                resultTitle = titles.mob;
+                resultImage = 'mob';
+                break;
+            case '-type1-action-niche-completed-serious':
+                resultTitle = titles.fate;
+                resultImage = 'fate';
+                break;
+            case '-type1-action-niche-completed-comedic-animation':
+                resultTitle = titles.fire;
+                resultImage = 'fire';
+            default:
+                resultTitle = titles.answer;
+                resultImage = answer;
+        }
     }
 }());
